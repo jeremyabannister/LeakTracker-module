822b166fb19a526ec08b531d4f03dc1b08500c39
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-09-26 12:26:22 +0200

    Added `LeakTracker`.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,30 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+///
 import PackageDescription
 
+///
 let package = Package(
     name: "LeakTracker-module",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LeakTracker-module",
-            targets: ["LeakTracker-module"]),
+            targets: ["LeakTracker-module"]
+        ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        
+        ///
         .target(
             name: "LeakTracker-module",
-            dependencies: []),
+            dependencies: []
+        ),
+        
+        ///
         .testTarget(
-            name: "LeakTracker-moduleTests",
-            dependencies: ["LeakTracker-module"]),
+            name: "LeakTracker-tests",
+            dependencies: ["LeakTracker-module"]
+        ),
     ]
 )
--- Sources/LeakTracker-module/LeakTracker_module.swift
+++
@@ -1,6 +0,0 @@
-public struct LeakTracker_module {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
