cd8a7dcbd917e12a269366ec5b5f1a3c816950e0
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-09-26 12:12:35 +0200

    Initial Commit

---
+++ Sources/LeakTracker-module/LeakTracker_module.swift
@@ -0,0 +1,6 @@
+public struct LeakTracker_module {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/LeakTracker-moduleTests/LeakTracker_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LeakTracker_module
+
+final class LeakTracker_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LeakTracker_module().text, "Hello, World!")
+    }
+}
